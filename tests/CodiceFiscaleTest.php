d06c1513bfad4f30245be15902e63940891a06e9
Nigro Simone <nigro.simone@gmail.com>
2022-01-21 23:33:53 +0100

    format

--- tests/CodiceFiscaleTest.php
+++ tests/CodiceFiscaleTest.php
@@ -1,72 +1,120 @@
 <?php
-
 require_once __DIR__ . "/../src/CodiceFiscale.php";
 use PHPUnit\Framework\TestCase;
 use NigroSimone\CodiceFiscale;
 
 class CodiceFiscaleTest extends TestCase
 {
+
     /**
+     *
      * @dataProvider goodDataProvider
      */
-    function test_checker_can_detect_omocodia($goodFiscalCode): void
+    function test_checker_can_detect_omocodia($goodFiscalCode)
     {
         $chk = new CodiceFiscale();
 
-        self::assertTrue(
-            $chk->validaCodiceFiscale($goodFiscalCode),
-            $chk->getErrore() ?? ""
-        );
+        self::assertTrue($chk->validaCodiceFiscale($goodFiscalCode), $chk->getErrore() ?? "");
     }
 
     /**
+     *
      * @dataProvider badDataProvider
      */
-    function test_checker_can_detect_badCodes($badFiscalCode): void
+    function test_checker_can_detect_badCodes($badFiscalCode)
     {
         $chk = new CodiceFiscale();
 
-        self::assertFalse(
-            $chk->validaCodiceFiscale($badFiscalCode),
-            $chk->getErrore() ?? ""
-        );
+        self::assertFalse($chk->validaCodiceFiscale($badFiscalCode), $chk->getErrore() ?? "");
     }
 
     public function goodDataProvider(): array
     {
         return [
-            ["MRARSS75P14H501I"],
-            ["MRARSS82M56F205J"],
-            ["LRNCST94B08F104C"],
-            ["LRNCST94B08F10QZ"],
-            ["LRNCST94B08F1L4N"],
-            ["LRNCST94B08FM04U"],
-            ["LRNCST94B0UF104Z"],
-            ["LRNCSTV4B08F104R"],
-            ["LRNCST94BL8F1LQV"],
-            ["LRNCSTVQB08F10QA"],
-            ["LRNCSTVQBLUFMLQL"],
-            ["BDLMMD80B13Z33SK"],
+            [
+                "MRARSS75P14H501I"
+            ],
+            [
+                "MRARSS82M56F205J"
+            ],
+            [
+                "LRNCST94B08F104C"
+            ],
+            [
+                "LRNCST94B08F10QZ"
+            ],
+            [
+                "LRNCST94B08F1L4N"
+            ],
+            [
+                "LRNCST94B08FM04U"
+            ],
+            [
+                "LRNCST94B0UF104Z"
+            ],
+            [
+                "LRNCSTV4B08F104R"
+            ],
+            [
+                "LRNCST94BL8F1LQV"
+            ],
+            [
+                "LRNCSTVQB08F10QA"
+            ],
+            [
+                "LRNCSTVQBLUFMLQL"
+            ],
+            [
+                "BDLMMD80B13Z33SK"
+            ]
         ];
     }
 
     public function badDataProvider(): array
     {
         return [
-            ["MRARSS82M56F205I"],
-            ["LRNCST94B08F104Z"],
-            ["LRNCST94B08F104"],
-            ["1RNCST94B08F104Z"],
-            ["XRNCST94B08F104C"],
-            ["LXNCST94B08F10QZ"],
-            ["LRXCST94B08F1L4N"],
-            ["LRNXST94B08FM04U"],
-            ["LRNCXT94B0UF104Z"],
-            ["LRNCSXV4B08F104R"],
-            ["LRNCSTX4BL8F1LQV"],
-            ["LRNCSTVXB08F10QA"],
-            ["LRNCSTVQXLUFMLQL"],
-            ["RNCSTVQXLUFMLQL"],
+            [
+                "MRARSS82M56F205I"
+            ],
+            [
+                "LRNCST94B08F104Z"
+            ],
+            [
+                "LRNCST94B08F104"
+            ],
+            [
+                "1RNCST94B08F104Z"
+            ],
+            [
+                "XRNCST94B08F104C"
+            ],
+            [
+                "LXNCST94B08F10QZ"
+            ],
+            [
+                "LRXCST94B08F1L4N"
+            ],
+            [
+                "LRNXST94B08FM04U"
+            ],
+            [
+                "LRNCXT94B0UF104Z"
+            ],
+            [
+                "LRNCSXV4B08F104R"
+            ],
+            [
+                "LRNCSTX4BL8F1LQV"
+            ],
+            [
+                "LRNCSTVXB08F10QA"
+            ],
+            [
+                "LRNCSTVQXLUFMLQL"
+            ],
+            [
+                "RNCSTVQXLUFMLQL"
+            ]
         ];
     }
 }
